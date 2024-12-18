44c5618241ad944785838ac6eb938a1b242dfac9
Samer Bagla <sbagla369@gmail.com>
2024-12-18 11:37:45 +0000

    add procfile

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='hotels/')
+
+class Room(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    room_type = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+
+
--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Gallery(models.Model):
+    hotel =  models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='gallery/')
+    caption = models.CharField(max_length=100, blank=True)
