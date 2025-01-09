e04ef65bbb8d6cf1984c16ba8bed5269ccf68f2c
Samer Bagla <sbagla369@gmail.com>
2025-01-09 13:15:44 +0000

    create database, superuser, update booking app, and home.html

--- hotel_booking/urls.py
+++ hotel_booking/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import booking.views as views 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('book/<int:room_id>/', views.book_room, name='book_room'),
+    path('',views.home, name='home'),
+    path('book/<int:room_id>/',views.book_room, name='book_room'),
 ]
